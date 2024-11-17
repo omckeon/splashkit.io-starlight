d6d647513d5aec1a9b755390676fdecc2db7b5d4
omckeon <ordmckeon@gmail.com>
2024-11-17 09:15:06 +0000

    update again

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://splashkit.github.io/',
-  // base: '/splashkit.io-starlight',  // if hosted without domain.
+  base: '/splashkit.io-starlight',  // if hosted without domain.
   // output: 'static',
   // outDir: './dist',
   // build: {
