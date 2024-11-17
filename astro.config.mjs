8d5754786769e7438769ed87c38a0d18e5eb7a16
omckeon <ordmckeon@gmail.com>
2024-11-17 08:38:07 +0000

    testing with github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://splashkit.io/',
-  // base: '/splashkit.io-starlight',  // if hosted without domain.
+  site: 'https://splashkit.github.io/',
+  base: '/splashkit.io-starlight',  // if hosted without domain.
   //   output: "server",
   //   adapter: netlify(),
   integrations: [
